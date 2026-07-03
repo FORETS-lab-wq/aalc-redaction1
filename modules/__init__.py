c1a1a6bed02b5c401c2b0f7a8209eac611b12b8b
FORETS-lab-wq <mathieu.forets@ecf-cerca.fr>
2026-07-03 09:34:58 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,15 +1,1 @@
-from modules import page_import
-from modules import page_individuel
-from modules import page_longitudinal
-from modules import page_transversal
-from modules import page_synthese
-from modules import page_export
-
-__all__ = [
-    "page_import",
-    "page_individuel",
-    "page_longitudinal",
-    "page_transversal",
-    "page_synthese",
-    "page_export",
-]
+# modules
